9a7373dfc23a38c7539efbb3f0b7129f98756cfe
Sarah Akinkunmi <hello.sakinkunmi@gmail.com>
2023-12-18 12:44:45 +0100

    corrected restaurant to hotel in hotel file

--- Hotels_TripAdvisor/hotel_model_building.py
+++ Hotels_TripAdvisor/hotel_model_building.py
@@ -85,7 +85,7 @@ if submit:
     if not user_input or not user_input.strip():
         st.error("The hotel name field is required")
     elif len(user_input) == 1:
-        st.error("Please type out full restaurant name")
+        st.error("Please type out full hotel name")
     else:    
         if not doesUserHotelExist:
             st.error(f'{user_input} is not in our database, we apologize about that.')
